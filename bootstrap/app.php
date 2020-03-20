1f64490b0880820ac4e69d8739b26c3fd34dd27e
Saymon Tavares <saymonweb@hotmail.com>
2020-03-20 13:06:07 -0300

    configure composer illuminate\mail

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -91,7 +91,20 @@ $app->routeMiddleware([
 
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Illuminate\Mail\MailServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+
+/*
+|--------------------------------------------------------------------------
+| Register the Mailer and its Aliases
+|--------------------------------------------------------------------------
+|
+*/
+
+$app->configure('mail');
+$app->alias('mailer', Illuminate\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
 
 /*
 |--------------------------------------------------------------------------
